9135e2279737e4f44dfdf0b00952c05085c02932
Gomez7373 <sgc.holberton@gmail.com>
2023-12-11 13:54:54 -0400

    try3

--- _SHELL.c
+++ _SHELL.c
@@ -4,11 +4,24 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 #include "main.h"
 
 void _SHELL(char *input) {
     pid_t pid;
-    char *argv[2];  /* Declare the array */
+    char **args;  /* Declare an array to store command and arguments */
+
+    /* Tokenize the input into command and arguments */
+    int arg_count = 0;
+    char *token = strtok(input, " ");
+    args = malloc(sizeof(char *));
+    while (token != NULL) {
+        args[arg_count] = token;
+        arg_count++;
+        args = realloc(args, (arg_count + 1) * sizeof(char *));
+        token = strtok(NULL, " ");
+    }
+    args[arg_count] = NULL;  /* Null-terminate the array */
 
     pid = fork();
 
@@ -19,16 +32,17 @@ void _SHELL(char *input) {
 
     if (pid == 0) {
         /* Child process */
-        argv[0] = input;
-        argv[1] = NULL;  /* Initialize the array */
-        if (execve(input, argv, (char *const *)NULL) == -1) {
-            perror("execve");
+        if (execvp(args[0], args) == -1) {
+            perror("execvp");
             exit(EXIT_FAILURE);
         }
     } else {
         /* Parent process */
         wait(NULL);
     }
+
+    /* Free the allocated memory */
+    free(args);
     printf("Executing: %s\n", input);
 }
 
