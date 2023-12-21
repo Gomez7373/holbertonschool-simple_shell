9f0e6eb1de3bef0aac461113facd2386e8e5765b
Gomez7373 <sgc.holberton@gmail.com>
2023-12-21 02:16:58 -0400

    betty

--- abdi_shell.c
+++ abdi_shell.c
@@ -11,143 +11,143 @@
 extern char **environ;
 
 /**
- * trim_whitespace - Trim leading and trailing whitespaces from a string
- * @str: Input string
- *
- * Return: Trimmed string
- */
+* trim_whitespace - Trim leading and trailing whitespaces from a string
+* @str: Input string
+*
+* Return: Trimmed string
+*/
 char *trim_whitespace(char *str)
 {
-    char *end;
+char *end;
 
-    /* Trim leading spaces */
-    while (isspace((unsigned char)*str))
-        str++;
+/* Trim leading spaces */
+while (isspace((unsigned char)*str))
+str++;
 
-    if (*str == 0)
-        return (str);
+if (*str == 0)
+return (str);
 
-    /* Trim trailing spaces */
-    end = str + strlen(str) - 1;
-    while (end > str && isspace((unsigned char)*end))
-        end--;
+/* Trim trailing spaces */
+end = str + strlen(str) - 1;
+while (end > str && isspace((unsigned char)*end))
+end--;
 
-    *(end + 1) = 0;
+*(end + 1) = 0;
 
-    return (str);
+return (str);
 }
 
 /**
- * get_command - Get user input for the command
- * @command: Buffer to store user input
- * @interactive: Flag indicating interactive mode
- *
- * Return: 1 if successful, 0 if there's an error
- */
+* get_command - Get user input for the command
+* @command: Buffer to store user input
+* @interactive: Flag indicating interactive mode
+*
+* Return: 1 if successful, 0 if there's an error
+*/
 int get_command(char *command, int interactive)
 {
-    if (interactive)
-        printf("$ ");
+if (interactive)
+printf("$ ");
 
-    if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
-        return (0);
+if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+return (0);
 
-    command[strcspn(command, "\n")] = 0;
+command[strcspn(command, "\n")] = 0;
 
-    return (1);
+return (1);
 }
 
 /**
- * execute_command - Execute a shell command
- * @full_command: Full command string
- * @last_status: Pointer to last command's exit status
- */
+* execute_command - Execute a shell command
+* @full_command: Full command string
+* @last_status: Pointer to last command's exit status
+*/
 void execute_command(char *full_command, int *last_status)
 {
-    char *argv[MAX_ARGS], *t;
-    int i = 0, s, j;
-    pid_t p = -1;
+char *argv[MAX_ARGS], *t;
+int i = 0, s, j;
+pid_t p = -1;
 
-    /* Splitting the command into arguments */
-    for (t = strtok(full_command, " ");
-         t && i < MAX_ARGS - 1; t = strtok(NULL, " "))
-        argv[i++] = t;
+/* Splitting the command into arguments */
+for (t = strtok(full_command, " ");
+t && i < MAX_ARGS - 1; t = strtok(NULL, " "))
+argv[i++] = t;
 
-    argv[i] = NULL;
+argv[i] = NULL;
 
-    /* Handling the 'env' command */
-    if (strcmp(argv[0], "env") == 0)
-    {
-        if (environ != NULL)
-        {
-            for (j = 0; environ[j] != NULL; ++j)
-            {
-                printf("%s\n", environ[j]);
-            }
-        }
-        return;
-    }
+/* Handling the 'env' command */
+if (strcmp(argv[0], "env") == 0)
+{
+if (environ != NULL)
+{
+for (j = 0; environ[j] != NULL; ++j)
+{
+printf("%s\n", environ[j]);
+}
+}
+return;
+}
 
-    /* Handling the 'exit' command */
-    else if (strcmp(argv[0], "exit") == 0)
-    {
-        exit(*last_status);
-    }
+/* Handling the 'exit' command */
+else if (strcmp(argv[0], "exit") == 0)
+{
+exit(*last_status);
+}
 
-    /* Handling other commands */
-    else
-    {
-        /* Check if PATH is set for external commands */
-        if (getenv("PATH") == NULL && strcmp(argv[0], "ls") == 0)
-        {
-            fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-            *last_status = 127;
-            return;
-        }
+/* Handling other commands */
+else
+{
+/* Check if PATH is set for external commands */
+if (getenv("PATH") == NULL && strcmp(argv[0], "ls") == 0)
+{
+fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
+*last_status = 127;
+return;
+}
 
-        /* Creating a child process */
-        p = fork();
-        if (p == -1)
-        {
-            perror("fork");
-        }
-        else if (p == 0)
-        {
-            execvp(argv[0], argv);
-            fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-            exit(127);
-        }
-        else
-        {
-            waitpid(p, &s, 0);
-            *last_status = WIFEXITED(s) ? WEXITSTATUS(s) : *last_status;
-        }
-    }
+/* Creating a child process */
+p = fork();
+if (p == -1)
+{
+perror("fork");
+}
+else if (p == 0)
+{
+execvp(argv[0], argv);
+fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
+exit(127);
+}
+else
+{
+waitpid(p, &s, 0);
+*last_status = WIFEXITED(s) ? WEXITSTATUS(s) : *last_status;
+}
+}
 }
 
 
 /**
- * main - Shell entry point
- *
- * Return: Last command exit status
- */
+* main - Shell entry point
+*
+* Return: Last command exit status
+*/
 int main(void)
 {
-    char command[MAX_COMMAND_LENGTH];
-    int interactive = isatty(STDIN_FILENO);
-    int last_status = 0;
+char command[MAX_COMMAND_LENGTH];
+int interactive = isatty(STDIN_FILENO);
+int last_status = 0;
 
-    while (get_command(command, interactive))
-    {
-        char *trimmed_command = trim_whitespace(command);
-        if (strlen(trimmed_command) > 0)
-        {
-            execute_command(trimmed_command, &last_status);
-        }
-    }
+while (get_command(command, interactive))
+{
+char *trimmed_command = trim_whitespace(command);
+if (strlen(trimmed_command) > 0)
+{
+execute_command(trimmed_command, &last_status);
+}
+}
 
-    if (interactive)
-        printf("\n");
+if (interactive)
+printf("\n");
 
-    return (last_status);
+return (last_status);
 }
