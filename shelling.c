7b467e5f3cc4d89971ff4a07e7e0399ac8c27969
Gomez7373 <sgc.holberton@gmail.com>
2023-12-20 22:46:27 -0400

    betty&task4

--- shelling.c
+++ shelling.c
@@ -4,13 +4,16 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <ctype.h>
-#include "main.h"
 
 #define MAX_COMMAND_LENGTH 1024
 #define MAX_ARGS 64
 
-
 extern char **environ;
+
+int get_command(char *command, int interactive);
+char *trim_whitespace(char *str);
+void execute_env(void);
+void execute_command(char *full_command, int *last_status);
 
 int main(void)
 {
@@ -39,46 +42,44 @@ int main(void)
     return last_status;
 }
 
-/*-------------------------------------------------*/
 int get_command(char *command, int interactive)
 {
-if (interactive)
-{
-printf("$ ");
-fflush(stdout);
+    if (interactive)
+    {
+        printf("$ ");
+        fflush(stdout);
+    }
+
+    if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+        return (0);
+
+    command[strcspn(command, "\n")] = 0;
+
+    return (1);
 }
 
-if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
-return (0);
-
-command[strcspn(command, "\n")] = 0;
-
-return (1);
-}
-/*---------------------------------------------------*/
 char *trim_whitespace(char *str)
 {
-char *end;
+    char *end;
 
-while (isspace((unsigned char)*str))
-str++;
+    while (isspace((unsigned char)*str))
+        str++;
 
-if (*str == 0)
-return str;
+    if (*str == 0)
+        return str;
 
-end = str + strlen(str) - 1;
-while (end > str && isspace((unsigned char)*end))
-end--;
+    end = str + strlen(str) - 1;
+    while (end > str && isspace((unsigned char)*end))
+        end--;
 
-*(end + 1) = 0;
+    *(end + 1) = 0;
 
-return (str);
+    return (str);
 }
-/*------------------------------------------------------*/
-void execute_env(char **envp)
+
+void execute_env(void)
 {
-    char **env_ptr = envp;
-    size_t length = 0;
+    char **env_ptr = environ;
 
     if (env_ptr == NULL)
     {
@@ -90,26 +91,25 @@ void execute_env(char **envp)
     while (*env_ptr != NULL)
     {
         printf("%s\n", *env_ptr);
-        length += strlen(*env_ptr) +1;
         env_ptr++;
     }
-    printf("](Length: %lu)\n", length);
+    printf("](Length: %lu)\n", (unsigned long)strlen(*environ));
     printf("(stderr)[](Length: 0)\n");
     printf("(status)[0]\n\n");
 
     printf("Environment:\n");
-    env_ptr = envp;
+    env_ptr = environ;
     while (*env_ptr != NULL)
     {
         printf("%s\n", *env_ptr);
         env_ptr++;
     }
-    printf("(Length: %lu)\n", (unsigned long)length);
+    printf("](Length: %lu)\n", (unsigned long)strlen(*environ));
     printf("(stderr)[](Length: 0)\n");
     printf("(status)[0]\n\n");
 }
+/*---------------------------------------------------------------*/
 
-/*-------------------------------------------------------*/
 void execute_command(char *full_command, int *last_status)
 {
 char *argv[MAX_ARGS], *t;
@@ -125,7 +125,7 @@ argv[i] = NULL;
 
 if (strcmp(argv[0], "env") == 0)
 {
-execute_env(environ);
+execute_env();
 }
 else if (strcmp(argv[0], "exit") == 0)
 {
@@ -141,9 +141,11 @@ exit(1);
 }
 else if (p == 0)
 {
-execvp(argv[0], argv);
-fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
+if (execvp(argv[0], argv) == -1)
+{
+perror("execvp");
 exit(127);
+}
 }
 else
 {
@@ -152,3 +154,4 @@ waitpid(p, &s, 0);
 }
 }
 }
+
